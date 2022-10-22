f058a99a5bd238ca1e22f5183b5cf66ab8c254d2
= <shimaa.k.badawy@gmail.com>
2022-10-22 14:19:10 -0400

    Add project files.

---
+++ AL0Y-IEEE488_2-Tester/mainForm.Designer.cs
@@ -0,0 +1,51 @@
+﻿namespace AL0Y_IEEE488_2_Tester
+{
+    partial class mainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // mainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(93)))), ((int)(((byte)(82)))), ((int)(((byte)(84)))));
+            this.ClientSize = new System.Drawing.Size(845, 561);
+            this.ForeColor = System.Drawing.Color.White;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "mainForm";
+            this.Text = "IEEE 488.2 Tester - By: Aly Badawy";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
+
